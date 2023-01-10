1c6ff4179781600e1d0b6c67b4cff65dc373453c
Magnus Engdahl <kaffebreak@gmail.com>
2023-01-10 16:13:06 +0100

    Basic player stuff

---
+++ TriviaRoyale/Client/Pages/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace TriviaRoyale.Client.Pages
+{
+	public class Player
+	{
+		public static int createdPlayers;
+		public int Points { get; set; }
+		public string Name { get; set; }
+
+		public Player()
+		{
+			createdPlayers++;
+		}
+		//public Player(string name)
+		//{
+		//	this.Name = name;
+		//}
+	}
+}
