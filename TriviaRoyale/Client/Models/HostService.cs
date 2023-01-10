fa59ba081250a6462f5ded4c5af68f2f5516ac88
Joakim Lignerkrona <65370969+joakim-lignerkrona@users.noreply.github.com>
2023-01-10 13:13:11 +0100

    lunch

---
+++ TriviaRoyale/Client/Models/HostService.cs
@@ -0,0 +1,11 @@
+﻿using TriviaRoyale.Shared;
+
+namespace TriviaRoyale.Client.Models
+{
+    public class HostService
+    {
+        public string RoomID { get; set; }
+        public List<Player> Players { get; set; }
+
+    }
+}
