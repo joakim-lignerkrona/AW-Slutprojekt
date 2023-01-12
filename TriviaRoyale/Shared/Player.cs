6b97c56e4c485a5322d13c0411c3c4eefd33d6bd
Magnus Engdahl <kaffebreak@gmail.com>
2023-01-12 10:16:19 +0100

    Ny branch, nytt innehåll

--- TriviaRoyale/Shared/Player.cs
+++ TriviaRoyale/Shared/Player.cs
@@ -1,19 +1,23 @@
-﻿namespace TriviaRoyale.Shared
-{
-    public class Player
-    {
-        public static int createdPlayers;
-        public string RoomID { get; set; }
-        public int Points { get; set; }
-        public string Name { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
 
-        public Player()
-        {
-            createdPlayers++;
-        }
-        public Player(string name)
-        {
-            this.Name = name;
-        }
-    }
+namespace TriviaRoyale.Shared
+{
+	public class Player
+	{
+		public static int createdPlayers;
+		public string RoomID { get; set; }
+		public int Points { get; set; }
+
+		[Required(ErrorMessage = "You must enter a name")]
+		public string Name { get; set; }
+
+		public Player()
+		{
+			createdPlayers++;
+		}
+		public Player(string name)
+		{
+			this.Name = name;
+		}
+	}
 }
