800a680a3c7c91a8a393befd276425b16a4a843e
Magnus Engdahl <kaffebreak@gmail.com>
2023-01-13 10:54:42 +0100

    d

--- TriviaRoyale/Server/Hubs/QuizHub.cs
+++ TriviaRoyale/Server/Hubs/QuizHub.cs
@@ -48,25 +48,21 @@ namespace TriviaRoyale.Server.Hubs
             return base.OnConnectedAsync();
         }
 
-        public async Task JoinRoom(string roomName)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+		public async Task JoinRoom(string roomName)
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
 
 			await Clients.Group(roomName).SendAsync("ServerLog", $"{Context.ConnectionId} has joined the group {roomName}.");
 		}
-            await Clients.Group(roomName).SendAsync("ReceiveAnswer", $"{Context.ConnectionId} has joined the group {roomName}.");
-        }
 
-        public async Task LeaveRoom(string roomName)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+		public async Task LeaveRoom(string roomName)
+		{
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
 
 			await Clients.Group(roomName).SendAsync("ServerLog", $"{Context.ConnectionId} has left the group {roomName}.");
 		}
-            await Clients.Group(roomName).SendAsync("ReceiveAnswer", $"{Context.ConnectionId} has left the group {roomName}.");
-        }
 
-        public async Task SendMessageToRoom(string roomName, string message)
+		public async Task SendMessageToRoom(string roomName, string message)
         {
             await Clients.Group(roomName).SendAsync("ReceiveAnswer", $"Message: {message}");
         }
