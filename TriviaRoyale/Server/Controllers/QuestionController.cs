e188bf336669dd80ac966b281f874864cef94798
Joakim Lignerkrona <jockan@icloud.com>
2023-01-18 23:12:33 +0100

    Working?

--- TriviaRoyale/Server/Controllers/QuestionController.cs
+++ TriviaRoyale/Server/Controllers/QuestionController.cs
@@ -4,36 +4,36 @@ using TriviaRoyale.Shared.Questions;
 
 namespace TriviaRoyale.Server.Controllers
 {
-    [Route("api/")]
-    [ApiController]
-    public class QuestionController : ControllerBase
-    {
+	[Route("api/")]
+	[ApiController]
+	public class QuestionController : ControllerBase
+	{
 
-        private readonly DataService service;
-        //Klienten
-        private readonly IWebHostEnvironment webHostEnvironment;
-        //Servern
-        private readonly IConfiguration configuration;
+		private readonly DataService service;
+		////Klienten
+		//private readonly IWebHostEnvironment webHostEnvironment;
+		////Servern
+		//private readonly IConfiguration configuration;
 
-        //DI
-        public QuestionController(DataService service, IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
-        {
-            this.service = service;
-            this.webHostEnvironment = webHostEnvironment;
-            this.configuration = configuration;
-        }
+		//DI
+		public QuestionController(DataService service /*IWebHostEnvironment webHostEnvironment, IConfiguration configuration*/)
+		{
+			this.service = service;
+			//this.webHostEnvironment = webHostEnvironment;
+			//this.configuration = configuration;
+		}
 
-        //CLIENT SIDE
-        [HttpGet("questions")]
-        public Question[] GetList()
-        {
-            return service.GetQuestions();
-        }
+		//CLIENT SIDE
+		[HttpGet("questions")]
+		public Question[] GetList()
+		{
+			return service.GetQuestions();
+		}
 
-        [HttpGet("hardquestions")]
-        public Question[] GetHardList()
-        {
-            return service.GetHardQuestions();
-        }
-    }
+		[HttpGet("hardquestions")]
+		public Question[] GetHardList()
+		{
+			return service.GetHardQuestions();
+		}
+	}
 }
