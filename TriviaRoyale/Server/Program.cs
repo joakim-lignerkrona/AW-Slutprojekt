e7f6e60427f26dea80f297232b591bbd6c6f01f1
Joakim Lignerkrona <jockan@icloud.com>
2023-01-18 23:32:53 +0100

    the dirty way

--- TriviaRoyale/Server/Program.cs
+++ TriviaRoyale/Server/Program.cs
@@ -44,11 +44,11 @@ namespace TriviaRoyale
             }
             else
             {
-                //app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
-            app.UseWebAssemblyDebugging();
+
 
             app.UseSwagger();
             app.UseSwaggerUI();
